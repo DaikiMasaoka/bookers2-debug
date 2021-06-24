0ba197ed6c16810f5230cce181977b81dd71ffea
DaikiMasaoka <masadai.29@icloud.com>
2021-06-24 07:56:34 +0000

    モデル追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def zipedit
-    params.require(:user).permit(:postcode, :prefecture_name, :address_city, :address_street, :address_building)
-  end
-
-
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def zipedit
+    params.require(:user).permit(:postcode, :prefecture_name, :address_city, :address_street, :address_building)
   end
 
   def ensure_correct_user

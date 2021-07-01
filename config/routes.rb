0a5fb5cbe146be8aae9a2ab298d241e064700b10
DaikiMasaoka <masadai.29@icloud.com>
2021-07-01 02:11:17 +0000

    DM

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   root 'homes#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update] do
+  resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show, :index]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
